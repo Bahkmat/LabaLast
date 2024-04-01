97bec17ffcad252674b01c2f6201c6c6f3e71917
Bahkmat <161296043+Bahkmat@users.noreply.github.com>
2024-04-01 14:24:07 +0500

    Add files via upload

---
+++ WpfApp13/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp13
+{
+    internal class Users
+    {
+        public static List<Users> user = new List<Users>();
+        public string Login { get; set; }
+        public int Password { get; set; }
+        public List<Users> users { get; set; }
+        public Users(string _Login, int _Password)
+        {
+            Login = _Login;
+            Password = _Password;
+            user = users;
+        }
+    }
+}
